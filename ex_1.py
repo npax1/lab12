c2e8c8516ff5bcc380057bc731ff47a61c5d07ad
npax1 <prakh55555@gmail.com>
2022-10-15 10:33:18 +0300

    add code+

--- ex_1.py
+++ ex_1.py
@@ -3,8 +3,8 @@ try:
     vel = int(input("розмір квадрату"))
     for i in range(vel):
         for j in range(vel):
-            print("*",and"")
-            print(' ')
+            print(f'*', end=" ")
+        print()
 except Exception as ex:
     print(f"error information:{ex}")
 finally:
